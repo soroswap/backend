a3a86dc8a37b1d106bc7189ff9204ba223ebaf69
Benjamin Guiloff <byguiloff@uc.cl>
2024-01-30 11:11:42 -0300

    add: function to subscribe to pairs

--- src/utils/keyXdr/getKeyXdrForPair.ts
+++ src/utils/keyXdr/getKeyXdrForPair.ts
@@ -1,11 +1,9 @@
 import * as sdk from 'stellar-sdk';
 
-export async function getKeyXdrForPair(pairIndex: number) {
+export function getKeyXdrForPair(pairIndex: number) {
   const indexScVal = sdk.nativeToScVal(Number(pairIndex), { type: "u32"});
   const vecScVal = sdk.xdr.ScVal.scvVec([sdk.xdr.ScVal.scvSymbol("PairAddressesNIndexed"), indexScVal])
   const key_xdr = vecScVal.toXDR('base64');
 
-  console.log("keyXdr:", key_xdr);
+  return key_xdr;
 };
-
-getKeyXdrForPair(1);
