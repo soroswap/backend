93bc9b3e2bb07b0be6e8b8ab4fe16c9d02784f90
Matias Poblete <matias.pobleteduran@gmail.com>
2024-01-31 18:45:37 -0300

    ✨ Add PairsService to PoolsService and implement getAllPools method

--- src/pools/pools.controller.ts
+++ src/pools/pools.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Get, Query, Body } from '@nestjs/common';
 import { PoolsService } from './pools.service';
+import { ApiOkResponse } from '@nestjs/swagger';
+import { NetworkApiQuery } from 'src/decorators';
+import { AllPoolsRequestBodyDto, AllPoolsResponseDto } from './dto/pools.dto';
+import { QueryNetworkDto } from 'src/dto';
 
 @Controller('pools')
 export class PoolsController {
-    constructor( private readonly poolsService: PoolsService) {
+    constructor( 
+        private readonly poolsService: PoolsService) {
 
     }
     @Get('/all')
     findAll() {
         return this.poolsService.findAll();
+    }
+
+    @ApiOkResponse({ description: 'return all pools', type: [AllPoolsResponseDto] })
+    @NetworkApiQuery()
+    @Post('/all_pools')
+    getAllPools(@Body() body: AllPoolsRequestBodyDto, @Query() query: QueryNetworkDto): Promise<AllPoolsResponseDto>{
+        return this.poolsService.getAllPools(body);
     }
 }
