a7f701d8d753f57122276917734cb407141a1e16
Benjamin Guiloff <byguiloff@uc.cl>
2024-02-06 13:24:44 -0300

    fix: check before subscribing to factory contract

--- scripts/subscribeToContract.ts
+++ scripts/subscribeToContract.ts
@@ -1,7 +1,8 @@
 import { Mercury } from 'mercury-sdk';
+import { PrismaClient } from '@prisma/client';
 import { getFactoryAddress } from '../src/utils/getFactoryAddress';
 
-(async function() {
+(async function () {
   const mercuryInstance = new Mercury({
     backendEndpoint: process.env.MERCURY_BACKEND_ENDPOINT,
     graphqlEndpoint: process.env.MERCURY_GRAPHQL_ENDPOINT,
@@ -9,24 +10,41 @@ import { getFactoryAddress } from '../src/utils/getFactoryAddress';
     password: process.env.MERCURY_TESTER_PASSWORD,
   });
 
-  const keyXdr = process.argv[2];
+  const prisma = new PrismaClient();
 
-  if (!keyXdr) {
-    console.error("You must provide a keyXdr as an argument.");
-    process.exit(1);
-  }
-  
+  const keyXdr = 'AAAAFA==';
+
   const contractId = await getFactoryAddress();
-  
-  const args = {
-    contractId,
-    keyXdr,
-    durability: "persistent"
-  }
-  
-  const subscribeResponse = await mercuryInstance.subscribeToLedgerEntries(args).catch((err) => {
-    console.error(err)
+
+  let subscriptionExists = await prisma.factorySubscription.findFirst({
+    where: {
+      contractId,
+    },
   });
-  
-  console.log(subscribeResponse);
-})()
+
+  if (!subscriptionExists) {
+    const args = {
+      contractId,
+      keyXdr,
+      durability: 'persistent',
+    };
+
+    const subscribeResponse = await mercuryInstance
+      .subscribeToLedgerEntries(args)
+      .catch((err) => {
+        console.error(err);
+      });
+
+    console.log(subscribeResponse);
+
+    const subscribeStored = await prisma.factorySubscription.create({
+      data: {
+        contractId,
+      },
+    });
+
+    console.log('Subscription stored in db', subscribeStored);
+  } else {
+    console.log('Already subscribed to factory contract', contractId);
+  }
+})();
