1ec2cad0b8ae675d28ee330a305c9a8fa4a89d91
Benjamin Guiloff <byguiloff@uc.cl>
2024-02-12 08:49:31 -0300

    added script to subscribe to phoenix contracts

---
+++ scripts/subscribeToPhoenixContracts.ts
@@ -0,0 +1,61 @@
+import { Mercury } from 'mercury-sdk';
+import { PrismaClient } from '@prisma/client';
+
+(async function () {
+  const mercuryInstance = new Mercury({
+    backendEndpoint: process.env.MERCURY_BACKEND_ENDPOINT,
+    graphqlEndpoint: process.env.MERCURY_GRAPHQL_ENDPOINT,
+    email: process.env.MERCURY_TESTER_EMAIL,
+    password: process.env.MERCURY_TESTER_PASSWORD,
+  });
+
+  const prisma = new PrismaClient();
+
+  const keyXdr = 'AAAAFA==';
+
+  const contractId = process.argv[2];
+  console.log('Contract ID:', contractId);
+
+  const protocol = process.argv[3];
+  console.log('Protocol:', protocol);
+
+  if (!contractId) {
+    console.error('Contract ID is required');
+    process.exit(1);
+  }
+
+  let subscriptionExists = await prisma.factorySubscription.findFirst({
+    where: {
+      contractId,
+    },
+  });
+
+  console.log('Subscription exists:', subscriptionExists);
+
+  if (!subscriptionExists) {
+    const args = {
+      contractId,
+      keyXdr,
+      durability: 'persistent',
+    };
+
+    const subscribeResponse = await mercuryInstance
+      .subscribeToLedgerEntries(args)
+      .catch((err) => {
+        console.error(err);
+      });
+
+    console.log(subscribeResponse);
+
+    const subscribeStored = await prisma.factorySubscription.create({
+      data: {
+        contractId,
+        protocol: 'phoenix',
+      },
+    });
+
+    console.log('Subscription stored in db', subscribeStored);
+  } else {
+    console.log('Already subscribed to factory contract', contractId);
+  }
+})();
