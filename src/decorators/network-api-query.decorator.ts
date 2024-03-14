2ef0f65386e5dc1f330f08ee2c7063944a2817bb
coderipper <hello@joaquinsoza.dev>
2024-03-14 16:03:16 +0100

    network query decorator

--- src/decorators/network-api-query.decorator.ts
+++ src/decorators/network-api-query.decorator.ts
@@ -4,8 +4,8 @@ import { Network } from '@prisma/client';
 export function NetworkApiQuery() {
   return ApiQuery({
     name: 'network',
-    required: false,
-    description: 'Network to use, defaults to testnet',
+    required: true,
+    description: 'Network to use <TESTNET | MAINNET>',
     enum: Network,
   });
 }
