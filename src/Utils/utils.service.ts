93186ac942b8b73f047923b49d99d6c0840daeed
Matias Poblete <matias.pobleteduran@gmail.com>
2024-04-03 21:07:52 -0300

    🚧Add router-sdk to calculate paths

--- src/Utils/utils.service.ts
+++ src/Utils/utils.service.ts
@@ -2,11 +2,20 @@ import {
   Inject,
   Injectable,
 } from '@nestjs/common';
+import {
+  Router,
+  Token,
+  CurrencyAmount,
+  TradeType,
+  Networks,
+} from "soroswap-router-sdk";
 import axios from 'axios';
 import { Cache } from 'cache-manager';
 import { PairsService } from 'src/pairs/pairs.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { fetchPathsDto } from './dto/fetchPaths.dto';
+import { configLoader } from '../config/config-loader';
+import { Protocols } from 'src/config/supportedProtocols';
 
 @Injectable()
 export class UtilsService {
@@ -17,15 +26,63 @@ export class UtilsService {
   ) {}
 
   async fetchPaths(network: string, body: fetchPathsDto) {
-    const { contractId0, contractId1 } = body;
+    console.log('✨fetchPaths');
+    console.log('⚠️network', network);
+    console.log('⚠️body', body);
+    const paths = await this.getPaths(network, body);
 
-    console.log(body)
-    /* const paths = await this.pairsModule.getPaths(
-      network,
-      contractId0,
-      contractId1,
+    return paths;
+  }
+
+  async getPaths(network: string, payload: fetchPathsDto) {
+    let currentNetwork = Networks.PUBLIC;
+    switch (network) {
+      case 'MAINNET':
+        currentNetwork = Networks.PUBLIC;
+        break;
+      case 'TESTNET':
+        currentNetwork = Networks.TESTNET;
+        break;
+      default:
+        throw new Error('Invalid network');
+    }
+
+    const amount = 10000000;
+
+    const asset0 = new Token(
+      currentNetwork,
+      payload.contractId0,
+      7,
+      "XLM",
+      "Stellar Lumens"
     );
 
-    return paths; */
+    const asset1 = new Token(
+      currentNetwork,
+      payload.contractId1,
+      7,
+      "XLM",
+      "Stellar Lumens"
+    );
+    
+    const router = new Router({
+      backendUrl: configLoader().backendURL,
+      backendApiKey: configLoader().apiKey,
+      pairsCacheInSeconds: 20,
+      protocols: [Protocols.SOROSWAP],
+      network: currentNetwork,
+    });
+
+    const currencyAmount = CurrencyAmount.fromRawAmount(asset1, amount);
+    const quoteCurrency = asset0;
+
+    const route = await router.route(
+      currencyAmount,
+      quoteCurrency,
+      TradeType.EXACT_INPUT
+    );
+
+    console.log(route.trade.path);
+    return route.trade.path;
   }
 }
